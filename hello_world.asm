b80584c075e494485df614e8a59791a6d80660a4
Christian Andersson <chran554@hotmail.com>
2020-10-12 20:36:42 +0000

    First commit

--- hello_world.asm
+++ hello_world.asm
@@ -1,25 +1,24 @@
-; Tell Relaunch64 editor what start address there is for the program
-; start=$1000
+// Tell Relaunch64 editor what start address there is for the program
+// start=$1000
 
-*=$0801
+*=$0801 "basic"
+    .byte $0C, $08
+    .byte $0A, $00, $9E, $20, $32, $30, $36, $34, $00        // BASIC: 10 SYS 2064      // #2064 = $0810
+    .byte $00, $00                                           // end of BASIC program
 
-  !byte $0c, $08, $0a, $00, $9e, $20
-  !byte $34, $30, $39, $36, $00, $00
-  !byte $00
+*=$0810 "main"
+    jsr $e544 // clear screen
 
-*=$1000
+    ldx #$00
+loop:
+    lda message,x
+    sta $0400,x
+    inx
+    cpx #$0c
+    bne loop
 
-  ldx #$00
-  
-loop 
-  lda message,x
-  and #$3f
-  sta $0400,x
-  inx
-  cpx #$0c
-  bne loop
-  
-  rts
-  
-message
-    !pet "Hello World!"
+    rts // return to basic
+
+message:
+    .encoding "screencode_upper"
+    .text "HELLO WORLD!"
